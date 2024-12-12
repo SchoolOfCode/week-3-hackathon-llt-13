8ca92ec79ff3d96427a1bc444ce8b67230bf863d
Luke Davies <luke.davies@junitech.com>
2024-12-12 11:44:10 +0000

    add fetch function and html text

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello!")
+
+async function fetchData() {
+      const response = await fetch('https://uselessfacts.jsph.pl//api/v2/facts/random?language=en', {
+        headers: {
+        Accept: "application/json",
+        },
+      });
+      const dailyFact = await response.json();
+      console.log(dailyFact);
+      return dailyFact;
+    }
+
