9cb850e256e1559dc0f50af95fc65fe5252c5dbc
Luke Davies <luke.davies@junitech.com>
2024-12-13 09:48:10 +0000

    fixes #25 #26 finish MVP 6

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello!")
-
 async function fetchData() {
   try {
     const response = await fetch('http://numbersapi.com/random/trivia', {
@@ -16,13 +14,38 @@ async function fetchData() {
     console.error('Error fetching data:', error);
 }};
 
-fetchData()
+
+async function fetchMath() {
+  try {
+    const response = await fetch('http://numbersapi.com/random/math', {
+      headers: {
+      "Accept": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const dailyMath = await response.text();
+    console.log(dailyMath)
+    return dailyMath;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+}};
 
 async function addFact() {
-  const factText = document.querySelector(".daily");
+  const factText = document.querySelector("#fact");
   factText.textContent = await fetchData();
+};
+
+async function addMath() {
+  const factText = document.querySelector("#mathFact");
+  factText.textContent = await fetchMath();
 };
 
 const button = document.getElementById("factButton");
 button.addEventListener("click", addFact);
-button.addEventListener("keypress", ".keyboard-clickable", addFact);
+button.addEventListener("keypress", addFact, ".keyboard-clickable");
+
+const buttontwo = document.getElementById("mathButton");
+buttontwo.addEventListener("click", addMath);
+buttontwo.addEventListener("keypress", addMath, ".keyboard-clickable"); // issue #25 and #26
